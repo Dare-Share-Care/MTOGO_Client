578f3dd7ac12ed37a31f49afbddeb4aebb588cf2
Julius Krüger Madsen <1230150922@protonmail.com>
2023-11-18 16:35:08 +0100

    renamed users to example to keep as a nice demo for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Users from './pages/Users';
+
+import Example from './pages/Example';
 
 const App = () => {
   return (
@@ -13,10 +11,10 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
+
+
+
+        <Route path="/example" element={<Example />} />
       </Routes>
     </Router>
   );
---
+++ src/models/Example.ts
@@ -0,0 +1,11 @@
+/**
+ * Example model
+ * @param {number} id - Example id
+ * @param {string} name - Example name
+ * @param {string} email - Example email
+ */
+export interface Example {
+    id: number;
+    name: string;
+    email: string;
+}
